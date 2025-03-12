756dc0fc554cb8d5c6a85663ba5d5fad54dce42d
Aashni Haria <49083909+aashniharia@users.noreply.github.com>
2025-03-12 14:01:35 +0000

    bug-fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import RecipeCategories from "./Components/RecipeCategories";
-import { Route, Routes } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import CategoryDetails from "./Components/CategoryDetails";
 import DeatailedRecipe from "./Components/DetailedRecipe";
 function App() {
@@ -10,10 +10,10 @@ function App() {
       <Header />
 
       <Routes>
-        <Route path="*" element={<RecipeCategories to="/" replace />} />
-        <Route path="/categories" exact element={<RecipeCategories />} />
-        <Route path="/categories/:id" exact element={<CategoryDetails />} />
-        <Route path="/categories/:id/:id" exact element={<DeatailedRecipe />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/categories" element={<RecipeCategories />} />
+        <Route path="/categories/:id" element={<CategoryDetails />} />
+        <Route path="/categories/:id/:id" element={<DeatailedRecipe />} />
       </Routes>
 
       <Footer />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,23 @@
 import React from "react";
-import axios from "axios";
+import { Link } from "react-router-dom";
 import { Container, Navbar, Nav } from "react-bootstrap";
 
 function Header() {
-
   return (
     <Navbar bg="dark" variant="dark" sticky="top" expand="lg" collapseOnSelect>
       <Container>
-        <Navbar.Brand href="/categories">Recipes For All</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/categories">
+          Recipes For All
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link className="nav-link" href="/categories">
+            <Nav.Link as={Link} to="/categories">
               Home
             </Nav.Link>
-            <Nav.Link href="/categories/Dessert"> Chef's Special: Dessert</Nav.Link>
+            <Nav.Link as={Link} to="/categories/Dessert">
+              Chef's Special: Dessert
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
